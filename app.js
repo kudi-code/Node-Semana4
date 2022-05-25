9973f0393c1d952a62fc1f1065a80761c8d76ca5
Kevin Casas <kevinsal.casas@gmail.com>
2022-05-24 20:22:08 -0500

    Ready for deploy Heroku

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express'); //Llamamos a express
 const cors = require('cors');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const compression = require('compression');
+const morgan = require('morgan');
+
 
 // Controllers
 const { globalErrorHandler } = require('./controllers/errors.controller');
@@ -15,6 +20,25 @@ app.use(cors());
 
 //Enable incoming JSON data
 app.use(express.json());
+
+// Add security headers
+app.use(helmet());
+
+// Compress responses
+app.use(compression());
+
+// Log incoming requests
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+else app.use(morgan('combined'));
+
+// Limit IP requests
+const limiter = rateLimit({
+    max: 10000,
+    windowMs: 1 * 60 * 60 * 1000, // 1 hr
+    message: 'Too many requests from this IP',
+  });
+
+app.use(limiter);
 
 //Endpoints
 app.use('/api/v1/users', usersRouter); //llamando a users
